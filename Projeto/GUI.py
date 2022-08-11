f9577991379a8bf2d13b79689d6878580ce7504b
Pedro <pedroandre.lcc@gmail.com>
2022-08-11 15:58:45 -0300

    GUI v0.2.1 - Small Updates

--- Projeto/GUI.py
+++ Projeto/GUI.py
@@ -1,9 +1,7 @@
 # Libraries
-from turtle import width
 from Dark_TitleBar import *
 from tkinter import *
 import os
-from PIL import ImageTk, Image
 
 class App(Tk):
 
@@ -19,7 +17,7 @@ class App(Tk):
         self.title("Dashboard Financeiro")
         self.geometry(f"{App.LARGURA}x{App.ALTURA}")
         self.resizable(False,False)
-        #self.configure(background='#36393f')
+        self.configure(background='#36393f')
         self.iconbitmap('Projeto\\image\\porco.ico')
         
         # Frames
@@ -31,6 +29,11 @@ class App(Tk):
         layout_1 = Frame(layout, width=App.LARGURA*0.8, height=App.ALTURA*0.33333, bg='#36393f').grid(row=0, column=1, sticky="nswe")
         layout_2 = Frame(layout, width=App.LARGURA*0.8, height=App.ALTURA*0.33333, bg='#36393f').grid(row=1, column=1, sticky="nswe")
         layout_3 = Frame(layout, width=App.LARGURA*0.8, height=App.ALTURA*0.33333, bg='#36393f').grid(row=2, column=1, sticky="nswe")
+
+        # Imagem
+        img_logo = PhotoImage(file='Projeto\\image\\Logo GipeTech.png')
+        
+        
 
 app = App()
 dark_title_bar(app)
