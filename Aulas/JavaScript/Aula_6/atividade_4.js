4a0b0f7cf059df7eb0709bc5eeb276dd048c08c5
Pedrolinho <pedroandre.lcc@gmail.com>
2022-10-14 21:52:56 -0300

    JavaScript - Aula 6

---
+++ Aulas/JavaScript/Aula_6/atividade_4.js
@@ -0,0 +1,10 @@
+function testNum(num) {
+    return new Promise((resolve, reject) => {
+        if(num >10) resolve(num);
+        else reject(num);
+    })
+}
+
+testNum(10)
+    .then(() => console.log("Maior que 10!"))
+    .catch(() => console.log("Menor ou igual a 10!"));
