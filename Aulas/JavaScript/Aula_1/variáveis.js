dc706a2bd039060a17a607c419b8f7bc48b9b7bc
Pedrolinho <pedroandre.lcc@gmail.com>
2022-10-25 03:08:09 -0300

    Update

---
+++ Aulas/JavaScript/Aula_1/variáveis.js
@@ -0,0 +1,13 @@
+/* const -> usado para strings ou boolean */
+/* let   -> usado para números */
+
+/* No JavaScript não é necessário o uso do ponto-e-vírgula (;) */
+const nome = "Pedro"
+const humano = true
+let idade = 20
+
+/* Para a impressão no console utiliza-se a função console.log() */
+console.log("Nome: " + nome + "; Idade: " + idade + "; Humano: " + humano)
+
+/* O uso de aspas simples invertidas (``) permitem o uso de uma string formatada utilizando ${variável} */
+console.log(`Nome: ${nome}; Idade: ${idade}; Humano: ${humano}`)
